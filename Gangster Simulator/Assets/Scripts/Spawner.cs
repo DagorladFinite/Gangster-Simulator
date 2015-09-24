a282a72ed782772a2e516e67ddb609158f0950d9
DagorladFinite <robert.coll.gd@gmail.com>
2015-09-24 14:22:21 +0200

    Insertar assets

--- Gangster Simulator/Assets/Scripts/Spawner.cs
+++ Gangster Simulator/Assets/Scripts/Spawner.cs
@@ -27,7 +27,7 @@ public class Spawner : MonoBehaviour {
 			cares1 = Instantiate (Cara,new Vector3(Posx,transform.position.y,transform.position.z),transform.rotation) as GameObject;
 			cares1.GetComponent<SpriteRenderer>().sprite = cares[Random.Range (0,16)];
 			cares1.transform.parent = transform;
-			cossos1 = Instantiate (Cos,new Vector3(Posx,transform.position.y - 0.48f,transform.position.z),transform.rotation) as GameObject;
+			cossos1 = Instantiate (Cos,new Vector3(Posx,transform.position.y - 0.40f,transform.position.z),transform.rotation) as GameObject;
 			cossos1.GetComponent<SpriteRenderer>().sprite = cossos[Random.Range (0,8)];
 			cossos1.transform.parent = transform;
 			

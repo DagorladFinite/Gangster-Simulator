6e304504f47a120327eb1e1dcf629d9764781b63
DagorladFinite <robert.coll.gd@gmail.com>
2015-10-05 17:54:43 +0200

    New Art Implementation
    New art rework i rediseny

---
+++ Gangster Simulator/Assets/Scripts/SkyManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyManager : MonoBehaviour {
+
+	public GameObject Nuvol;
+	public int num_nuvols;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i<num_nuvols; i++) {
+			float Posy = Random.Range (2.5f, 5.0f);
+			float Posx = Random.Range (-10.0f, 10.0f);
+			Nuvol = Instantiate (Nuvol, new Vector3 (Posx, Posy, 0), Nuvol.transform.rotation) as GameObject;
+			Nuvol.transform.localScale = new Vector3 (3,3,1);
+			Nuvol.transform.SetParent(transform);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

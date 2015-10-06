60f034dccc81d7efee02139771b5c18b3bc4feb1
DagorladFinite <robert.coll.gd@gmail.com>
2015-10-06 12:35:35 +0200

    Refactor of the upgrades
    Ara es mes facil posar nous upgrades

--- Gangster Simulator/Assets/Scripts/LayerManager.cs
+++ Gangster Simulator/Assets/Scripts/LayerManager.cs
@@ -13,6 +13,7 @@ public class LayerManager: MonoBehaviour {
 	public Color aff;
 	public int id;
 	public float scale = 0.0001f;
+	private bool clicked = true;
 
 
 
@@ -40,14 +41,17 @@ public class LayerManager: MonoBehaviour {
 	}
 	
 	public void PurchasedUpgrade(){
-		if (click.gold >= cost && click.canclick == true) {
+		if (click.gold >= cost && click.canclick == true && clicked == true) {
 			click.gold -= cost;
 			purchased = true;
 			foreach (Transform child in transform)
 			{
 				child.gameObject.SetActive(true);
 			}
-			this.transform.localScale = new Vector3(0.001F,0.001F,0.001F);
+			//this.transform.localScale = new Vector3(0.001F,0.001F,0.001F);
+			this.GetComponent<CanvasRenderer>().SetAlpha(0);
+			this.GetComponent<Button>().interactable = false;
+			clicked = false;
 		}
 	}
 	public void PurchasedLoad(){
@@ -56,7 +60,9 @@ public class LayerManager: MonoBehaviour {
 		{
 			child.gameObject.SetActive(true);
 		}
-		this.transform.localScale = new Vector3(0.001F,0.001F,0.001F);
+		this.GetComponent<CanvasRenderer>().SetAlpha(0);
+		clicked = false;
+		this.GetComponent<Button>().interactable = false;
 	}
 
 }

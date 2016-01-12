2aea797b47669ab2f24e96edacfee4f587bf9413
DagorladFinite <robert.coll.gd@gmail.com>
2016-01-12 17:06:58 +0100

    New UI

--- Gangster Simulator/Assets/Scripts/Feedback.cs
+++ Gangster Simulator/Assets/Scripts/Feedback.cs
@@ -12,15 +12,25 @@ public class Feedback : MonoBehaviour {
     private GameObject meta;
     public float time;
 	int duracio = 75;
+	int dur = 100;
+	public int SortLayer = 0;
+	//public int SortingLayerID = SortingLayer.GetLayerValueFromName("Pjs");
     // Use this for initialization
     void Start()
     {
+		Renderer renderer = this.gameObject.GetComponent<Renderer>();
+		if(renderer != null)
+		{
+			renderer.sortingOrder = SortLayer;
+			renderer.sortingLayerName = "Backgrounds2";
+
+		}
         //Click click = transform.Find ("Click").GetComponent<Click> () as Click;
         text.text = FormatNumber(input);//click.goldperclick.ToString();
 		Vector2 rand = new Vector2(UnityEngine.Random.Range(-75, 75), UnityEngine.Random.Range(100, 300));
         this.GetComponent<Rigidbody2D>().AddForce(rand);
         //transform.rotation = transform.rotation * Quaternion.Euler(Random.Range(-10,10),0,0);
-        meta = GameObject.FindGameObjectWithTag("Display");
+        meta = GameObject.Find("GoldDisplay");
     }
 	// Update is called once per frame
 	void Update () {
@@ -29,8 +39,13 @@ public class Feedback : MonoBehaviour {
             Vector2 pos = Vector2.MoveTowards(transform.position, meta.transform.position, time);
             transform.position = pos;
 
-            if (transform.position == meta.transform.position)
-            { Destroy(gameObject); }
+            if (dur == 0)
+            { 
+				Destroy(gameObject);
+				dur = 75;
+				//Destroy(gameObject); transform.position.y == meta.transform.position.y
+			}
+			dur--;
         }
 
         else
